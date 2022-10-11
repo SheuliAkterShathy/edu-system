10b49905b8aeff9e0a1d023c5e92d2f09a4a5df2
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-10-11 23:33:18 +0600

    logo-add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,7 @@ const Header = () => {
   <div className=" navbar-center md:navbar-start">
     <div className="dropdown">
       <label tabIndex={0} className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-8" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </label>
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 font-bold">
         <li><Link to ='/'>Home</Link></li>
@@ -22,6 +22,9 @@ const Header = () => {
       </ul>
     </div>
     
+   <img className='w-7 rounded-3xl mx-3' src=" https://c8.alamy.com/comp/2BKGC4H/bright-education-logo-2BKGC4H.jpg" alt="" />
+
+
     <Link to='/' className="btn btn-ghost normal-case text-xl font-bold">EduSystem</Link>
     
   </div>
