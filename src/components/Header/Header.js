4e0e25ed7f4cf59d25c562a7c502ff00791fb83a
Sheuli Akter Shathy <sheuliakter85@gmail.com>
2022-10-11 22:58:57 +0600

    favicon-add

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,7 +13,7 @@ const Header = () => {
       <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
         <li><Link to ='/'>Home</Link></li>
         <li tabIndex={0}>
-          <Link to='/topics' className="justify-between">
+          <Link to='/' className="justify-between">
             Topics
             {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z"/></svg> */}
           </Link>
@@ -32,7 +32,7 @@ const Header = () => {
     <ul className="menu menu-horizontal p-0">
       <li><Link to ='/'>Home</Link></li>
       <li tabIndex={0}>
-        <Link to='/topics'>
+        <Link to='/'>
           Topics
           {/* <svg className="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24"><path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z"/></svg> */}
         </Link>
